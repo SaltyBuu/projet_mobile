f230c00a3db0b704c11329a35f22c322cae47e61
tibo <dornthibault@gmail.com>
2021-05-25 20:44:55 +0200

    Rearrangement code

--- App.js
+++ App.js
@@ -1,115 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, SafeAreaView, TouchableWithoutFeedback, ActivityIndicator, Platform } from 'react-native';
-import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks'
-import { Icon } from 'react-native-elements'
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+import { Icon } from 'react-native-elements';
+import {MQTT }from 'sp-react-native-mqtt';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import graphScreen from './app/screens/graphScreen';
 //import { Client, Message } from 'react-native-paho-mqtt';
 //import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 export default function App() {
   const handlePress = () => console.log("text pressed");
   const orientation = useDeviceOrientation();
-  console.log(StatusBar.currentHeight)
+  console.log(StatusBar.currentHeight);
 
 
+  // /* create mqtt client */
+  // MQTT.createClient({
+  //   host: '127.0.0.1',
+  //   clientId: 'your_client_id'
+  // }).then(function(client) {
 
-  return (
-    
-    <SafeAreaView style={styles.container}>
-      <View style={styles.bandeSup}>
-          <Icon 
-          name='chevron-left'
-          type='feather'
-          color='white'
-          />
+  //   client.on('closed', function() {
+  //     console.log('mqtt.event.closed');
+  //   });
 
-          <Text style={styles.titre} onPress={handlePress}> Graphiques serveurs</Text>
+  //   client.on('error', function(msg) {
+  //     console.log('mqtt.event.error', msg);
+  //   });
 
-          <Icon
-          name='menu'
-          type='feather'
-          color='white'
-          />
-          
-      </View >
-      
-      
-        <View style={styles.MainRubriques}>
-          <View style={styles.rubriques} >
-            <Text>
-              Ram
-            </Text>
-          </View>
+  //   client.on('message', function(msg) {
+  //     console.log('mqtt.event.message', msg);
+  //   });
 
-          <View style={styles.rubriques}>
-          <Text>
-            CPU Usage
-          </Text>
-          </View>
+  //   client.on('connect', function() {
+  //     console.log('connected');
+  //     client.subscribe('data', 0);
+  //     //client.publish('data', "test", 0, false);
+  //   });
 
-          <View style={styles.rubriques}>
-          <Text>
-            CPU Temp
-          </Text>
-          </View>
-          
-          <View style={styles.rubriques}>
-          <Text>
-            Disk Storage
-          </Text>
-          </View>
-        </View>
-      
-      {/* <Text numberOfLines={2} onPress={handlePress}>mtn faut coder lol</Text> */}
-      {/* <StatusBar style="auto" /> */}
-    </SafeAreaView>
+  //   client.connect();
+  // }).catch(function(err){
+  //   console.log(err);
+  // });
 
-    
-  );
+  return (<graphScreen />);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // display: 'flex',
-    // flexDirection: 'row',
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    //alignItems: 'center',
-    //justifyContent: 'space-between',
-  },
-  bandeSup: {
-    backgroundColor: '#63003c',
-    width: '100%',
-    height: '30%',
-    display: 'flex',
-    flexDirection: 'row',
-    // textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-
-  },
-  titre: {
-    color: 'white',
-    display: 'flex',
-    textAlign: 'center',
-    fontSize: 20
-  },
-  rubriques: {
-    
-    flex: 0.25,
-    alignItems: 'center',
-    borderWidth: 2,
-    borderRadius: 2,
-    borderColor: '#63003c',
-  
-  },
-  MainRubriques: {
-    display: 'flex',
-    flexDirection: 'row',
-
-  }
-
-});
 
 
---
+++ app/screens/graphScreen.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TouchableWithoutFeedback, ActivityIndicator, Platform } from 'react-native';
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+import { Icon } from 'react-native-elements';
+
+function graphScreen(props) {
+    
+    return (
+        <SafeAreaView style={styles.container}>
+      <View style={styles.bandeSup}>
+          <Icon 
+          name='chevron-left'
+          type='feather'
+          color='white'
+          />
+
+          <Text style={styles.titre} onPress={handlePress}> Graphiques serveurs</Text>
+
+          <Icon
+          name='menu'
+          type='feather'
+          color='white'
+          />
+          
+      </View >
+      
+      
+        <View style={styles.MainRubriques}>
+          <View style={styles.rubriques} >
+            <Text>
+              Ram
+            </Text>
+          </View>
+
+          <View style={styles.rubriques}>
+          <Text>
+            CPU Usage
+          </Text>
+          </View>
+
+          <View style={styles.rubriques}>
+          <Text>
+            CPU Temp
+          </Text>
+          </View>
+          
+          <View style={styles.rubriques}>
+          <Text>
+            Disk Storage
+          </Text>
+          </View>
+        </View>
+      
+      {/* <Text numberOfLines={2} onPress={handlePress}>mtn faut coder lol</Text> */}
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+      // display: 'flex',
+      // flexDirection: 'row',
+      backgroundColor: '#fff',
+      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+      //alignItems: 'center',
+      //justifyContent: 'space-between',
+    },
+    bandeSup: {
+      backgroundColor: '#63003c',
+      width: '100%',
+      height: '30%',
+      display: 'flex',
+      flexDirection: 'row',
+      // textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+  
+    },
+    titre: {
+      color: 'white',
+      display: 'flex',
+      textAlign: 'center',
+      fontSize: 20
+    },
+    rubriques: {
+      
+      flex: 0.25,
+      alignItems: 'center',
+      borderWidth: 2,
+      borderRadius: 2,
+      borderColor: '#63003c',
+    
+    },
+    MainRubriques: {
+      display: 'flex',
+      flexDirection: 'row',
+  
+    }});
+export default graphScreen;
