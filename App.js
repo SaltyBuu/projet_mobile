b8d90c46eab905651bacf4c18b563c6211b31d50
tibo <dornthibault@gmail.com>
2021-06-11 03:06:41 +0200

    tout cassé et reconstruit avec template minimal

--- App.js
+++ App.js
@@ -6,40 +6,16 @@ import {MQTT }from 'sp-react-native-mqtt';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import GraphScreen from './app/screens/GraphScreen';
+import MQTTConnection from './app/config/MQTTCOnnection';
 //import { Client, Message } from 'react-native-paho-mqtt';
 //import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 export default function App() {
   
-
-  // /* create mqtt client */
-  // MQTT.createClient({
-  //   host: '127.0.0.1',
-  //   clientId: 'your_client_id'
-  // }).then(function(client) {
-
-  //   client.on('closed', function() {
-  //     console.log('mqtt.event.closed');
-  //   });
-
-  //   client.on('error', function(msg) {
-  //     console.log('mqtt.event.error', msg);
-  //   });
-
-  //   client.on('message', function(msg) {
-  //     console.log('mqtt.event.message', msg);
-  //   });
-
-  //   client.on('connect', function() {
-  //     console.log('connected');
-  //     client.subscribe('data', 0);
-  //     //client.publish('data', "test", 0, false);
-  //   });
-
-  //   client.connect();
-  // }).catch(function(err){
-  //   console.log(err);
-  // });
+  const client = new Paho.MQTT.Client('localhost', 9001, 'uname');
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  client.connect({ onSuccess:onConnect, useSSL: true });
 
   return <GraphScreen />;
 }
