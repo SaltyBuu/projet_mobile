df3f8cbf9bc3bdcae71ab6b441519146d869c17c
tibo <dornthibault@gmail.com>
2021-06-11 03:10:04 +0200

    MQTTConnection

---
+++ app/config/MQTTConnection.js
@@ -0,0 +1,26 @@
+import init from 'react_native_mqtt';
+import { AsyncStorage} from '@react-native-community/async-storage';
+
+init({
+  size: 10000,
+  storageBackend: AsyncStorage,
+  defaultExpires: 1000 * 3600 * 24,
+  enableCache: true,
+  reconnect: true,
+  sync : {
+  }
+});
+
+function onConnect() {
+  console.log("onConnect");
+}
+
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
